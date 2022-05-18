484ceb6b207fd1964a83ea3745a142365e67cab0
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-05-18 12:31:28 -0500

    Fix application helper bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
-      base title
+      base_title
     else 
-      "#{page_title} | #{base_title}"
+      page_title + " | " + base_title
     end
   end
 end
