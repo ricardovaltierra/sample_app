94d3917fe7e7a1a586ad885fb18e2a88a072e712
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-05-24 15:58:53 -0500

    Set test for valid signup information and place content_tag in flash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
+
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      # Generates a post request to the server, to users_path, so redirecting
+      # to the users controller and the create action (beacuse POST), taking
+      # the data for the user creation from the incoming params hash
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!              # The test will catch an eye where the site goes after
+                                  # submission
+    assert_template 'users/show'  # As usual learning Rails, after submitting any form
+                                  # the next page has the view with the detailed info
+                                  # about what was just saved (i.e. Profile page)
+    assert_not flash.empty?
+  end
 end
