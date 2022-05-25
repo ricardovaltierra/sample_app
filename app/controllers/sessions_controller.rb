4271a3fb59a3f1c1f8032bb16d27e4e4100d2cb6
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-05-25 12:55:52 -0500

    Create log in method on sessions helper and complete create action with redirecting to user profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
     end
-    render 'new', status: :unprocessable_entity
   end
 
   def destroy
