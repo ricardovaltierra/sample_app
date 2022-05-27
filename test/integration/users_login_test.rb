f58e0d3d18c2e6fc4b92a267e3fdb694e24b94a6
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-05-27 16:32:15 -0500

    Set integration test for remember_token working, with helper method and controller helper method

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -34,6 +34,21 @@ class ValidLogin < UsersLogin
   end
 end
 
+class RememberingTest < UsersLogin
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie from above is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
+end
+
 class ValidLoginTest < ValidLogin
 
   test "valid login" do
