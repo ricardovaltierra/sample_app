73b77bf0676f2219250db78da5ffa9a5d89a20d5
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-05-31 10:52:52 -0500

    Implement successful edit of user ifo with TDD, updating user model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      # Handling a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit', status: :unprocessable_entity
     end
