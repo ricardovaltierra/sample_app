5635130ffd1ab45ce6d441900d2fb1514dfd3829
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-06-03 12:25:10 -0500

    Create migration for user account activation and set before create actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -58,4 +59,17 @@ class User < ApplicationRecord
       SecureRandom.urlsafe_base64
     end
   end
+
+  private
+
+    # Converts email to all lowercase.
+    def downcase_email
+      self.email = email.downcase!
+    end
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
