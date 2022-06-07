d4a486b1f348723655f342bd6417b7d6dc543646
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-06-07 12:08:23 -0500

    Fix error on User model authenticated method and set account_activation edit action

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,16 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by email: params[:email]
+    # Below, params[:id] should contain the activation token to be used
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else # In the case the token can not be authenticated
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
 end
