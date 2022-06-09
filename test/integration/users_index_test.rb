c43de83a136500fd8096cdbfeb1e4b34e7d83dab
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-06-09 13:23:24 -0500

    Refactor users index integration test for testing activated state, adding unactive user in fixtures

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,24 +1,42 @@
 require "test_helper"
 
-class UsersIndexTest < ActionDispatch::IntegrationTest
+class UsersIndex < ActionDispatch::IntegrationTest
 
   def setup
     @admin      = users(:michael)
     @non_admin = users(:archer)
   end
+end
 
-  test "index as admin including pagination and delete links" do
+class UsersIndexAdmin < UsersIndex
+  def setup
+    super
     log_in_as(@admin)
     get users_path
+  end
+end
+
+class UsersIndexAdminTest < UsersIndexAdmin
+  
+  test "should render the index page" do
     assert_template 'users/index'
+  end
+
+  test "should paginate users" do
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+  end
+
+  test "should have delete links" do
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
     end
+  end
+
+  test "should be able to delete non-admin user" do
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
       assert_response :see_other
@@ -26,6 +44,24 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "should display only activated users" do
+    # Deactivate thi first user on the page.
+    # Making an inactive user fixture is not
+    # sufficient because Rails can't guarantee
+    # it would appear on the first page.
+    User.paginate(page: 1).first.toggle!(:activated)
+    # Ensure that all the displayed users are
+    # activated.
+    # assigns(:users) is taking @users variable
+    # coming form the controller, that will be
+    # used to fill the view
+    assigns(:users).each do |user|
+      assert user.activated?
+    end
+  end
+end
+
+class UsersNonAdminIndexTest < UsersIndex
   test "index as non-admin" do
     log_in_as(@non_admin)
     get users_path
