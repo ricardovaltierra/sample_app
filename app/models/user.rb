6ad5872a6f8d24fbc07ad02ec4f775815101b531
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-06-13 17:46:17 -0500

    Fill password resets edit view and add password_reset_expired? User model method

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,6 +85,11 @@ class User < ApplicationRecord
     end
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
     # Converts email to all lowercase.
