6ad23fee817aaa7e408d0369b71ab83dfdbae91f
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-06-24 12:22:17 -0500

    Set assertions for user profile with stats partial

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -4,6 +4,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   include ApplicationHelper
   def setup
     @user = users(:michael)
+
+    @user.following << users(:archer)
   end
 
   test "profile display" do
@@ -12,6 +14,10 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'title', full_title(@user.name) # To note here how we gained access to the 'full title' helper method, thanks to including the ApplicationHelper on the first line above.
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar' # We're going to find an img tag with 'gravatar' class nested in the h1.
+    assert_select 'a[href=?]', "/users/#{@user.id}/following",
+                               text: /#{@user.following.count}\s+following/
+    assert_select 'a[href=?]', "/users/#{@user.id}/followers",
+                               text: /#{@user.followers.count}\s+follower/
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination', count: 1
     @user.microposts.paginate(page: 1).each do  |micropost|
